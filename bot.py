122f3d10baffcceb907f51f863019efd329f8309
Andrew Hase <oldm8reggie@gmail.com>
2019-10-20 10:58:21 +0200

    gathering out top level comments

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 # Reddit Bot First Run
 import praw
+import pprint
+
+log = open("data.txt","w")
 
 #https://www.reddit.com/prefs/apps
 
@@ -9,9 +12,19 @@ reddit = praw.Reddit(client_id='JL53WY5Cw2WK8g',
                      user_agent='popularWordBot Script',
                      username='popularWordBot')
 
+sub = reddit.subreddit('testingground4bots')
+# for submission in reddit.subreddit('testingground4bots').hot(limit=10):
+#     print(submission)
+postID =[]
+for submission in sub.hot(limit=25):
 
-test = reddit.subreddit('testingground4bots')
-for submission in reddit.subreddit('testingground4bots').hot(limit=10):
-    print(submission)
+   postID.append(submission.id)
 
 
+#print(postID) #Suc
+for id in postID:
+   sub = reddit.submission(id=id)
+   for top_level_comment in sub.comments:
+    print(top_level_comment.body)
+
+   
