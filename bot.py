2cc9b54da9ebaf42cf5aa289f7975d5b58bfc252
Andrew Hase <oldm8reggie@gmail.com>
2019-10-20 14:25:22 +0200

    test works as expected

--- bot.py
+++ bot.py
@@ -12,52 +12,62 @@ reddit = praw.Reddit(client_id='JL53WY5Cw2WK8g',
                      user_agent='popularWordBot Script',
                      username='popularWordBot')
 
-sub = reddit.subreddit('politics')
+sub = reddit.subreddit('testingground4bots')
 
-# for submission in reddit.subreddit('testingground4bots').hot(limit=10):
-#     print(submission)
 
-postID =[]
 def gatherHotPage(limit):
+   postID = []
    for submission in sub.hot(limit=limit):
-
+      loging = open(submission.id+'.txt', 'w+',encoding="utf-8")
+      loging.write(str(submission.title)) 
+      loging.close()
       postID.append(submission.id)
-
-#gatherHotPage(25)
-#print(postID) #Suc
-
-
-#djf0n7
-#loging = open('comments.txt', 'w+')     
-#loging.write(str(fullList))
-#loging.close()
-loging = open('comments2.txt', 'w+',encoding="utf-8")         
-sub = reddit.submission(id='dk962x')
-sub.comments.replace_more(limit=None)
-for comment in sub.comments.list():
      
-     #fullList.append(comment.body)
-    # print(20 * "-")
-     loging.write(str(20 * "-"))
-     loging.write(str('\n'))
-    # print('ID: ',comment.id)
-     loging.write('ID: '+str(comment.id)+'\n')
-     #print('Parent ID: ', comment.parent())
-     loging.write('Parent ID: '+ str(comment.parent())+'\n')
-     #print(comment.body)
-     loging.write(str(comment.body)+'\n')
+   return postID
 
-   #   if len(comment.replies) > 0:
-   #      for reply in comment.replies:
-   #          print('Reply: ',reply.body)
 
-#loging = open('comments.txt', 'w+')     
-#loging.write(str(fullList))
-#loging.close()    
 
-# for id in postID:
-#    sub = reddit.submission(id=id)
-#    for top_level_comment in sub.comments:
-#     print(top_level_comment.body)
+def readComments(submissionID):
+   
+   sub = reddit.submission(id=submissionID)
+   sub.comments.replace_more(limit=None)
+   loging = open(submissionID+'.txt', 'a+',encoding="utf-8") #encoding needed to deal with emojis
+   for comment in sub.comments.list():
+      loging.write(str(20 * "-"))
+      loging.write(str('\n'))
+      loging.write('ID: '+str(comment.id)+'\n')
+      loging.write('Parent ID: '+ str(comment.parent())+'\n')
+      loging.write(str(comment.body)+'\n')
+   loging.close()
+
+##Main Test
+def dataGather():
+   analyise = gatherHotPage(25)
+   for data in analyise:
+      readComments(data)
+
+
+dataGather()
+   
+
+
+
+# loging = open('comments2.txt', 'w+',encoding="utf-8")         
+# sub = reddit.submission(id='dk962x')
+# sub.comments.replace_more(limit=None)
+# for comment in sub.comments.list():
+     
+#      #fullList.append(comment.body)
+#     # print(20 * "-")
+#      loging.write(str(20 * "-"))
+#      loging.write(str('\n'))
+#     # print('ID: ',comment.id)
+#      loging.write('ID: '+str(comment.id)+'\n')
+#      #print('Parent ID: ', comment.parent())
+#      loging.write('Parent ID: '+ str(comment.parent())+'\n')
+#      #print(comment.body)
+#      loging.write(str(comment.body)+'\n')
+
+ 
 
    
