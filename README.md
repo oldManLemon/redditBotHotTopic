4fff25dd2b45b0e46a62dc7778abb7428c7cddef
Andrew Hase <oldm8reggie@gmail.com>
2019-10-21 13:05:47 +0200

    run over r/australia succesful with results

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reddit Bot Hot Topic
+This bot will scan selected subs to analysise the most popular words/topics for the sub. It collect this data for a month then post the results to various subs letting them know their most popular words/topics.
+
+Written in Python
--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
 # Reddit Bot First Run
 import praw
+# Inbuilts
 import os
-import password
+from collections import Counter
 
+# just import the password
+import password
 
 
 # https://www.reddit.com/prefs/apps
 
 reddit = praw.Reddit(client_id='JL53WY5Cw2WK8g',
                      client_secret='fKqCZ44PIIFdqOjemTIKab0BfEs',
-                     #password='popularWordBot!',
+                     # password='popularWordBot!',
                      password=password.password,
                      user_agent='popularWordBot Script',
                      username='popularWordBot')
 
-sub = reddit.subreddit('testingground4bots')
+sub = reddit.subreddit('australia')
 
 
 def gatherHotPage(limit):
@@ -33,6 +36,7 @@ def readComments(submissionID):
     sub = reddit.submission(id=submissionID)
     sub.comments.replace_more(limit=None)
     # encoding needed to deal with emojis
+    # may use another method of getting data.
     loging = open(submissionID+'.txt', 'a+', encoding="utf-8")
     for comment in sub.comments.list():
         loging.write(str(20 * "-"))
@@ -49,46 +53,53 @@ def dataGather():
     analyise = gatherHotPage(25)
     for data in analyise:
         readComments(data)
+def listOfData():
+    listOfFilenames = []
+    for filename in os.listdir():
+        if filename.endswith('txt'):
+            listOfFilenames.append(filename)
+    return listOfFilenames
 
-
-dataGather()
 
 def wordCounter():
     # print(os.listdir())
-   wordCount = {}
-   
-   for filename in os.listdir():
-      print(filename)
-      if filename.endswith('txt'):
-         thread = open(filename, 'r+', encoding="utf-8")
-         for word in thread.read().split():
-            word =  word.lower()
+    wordCount = {}
+    dataFiles = listOfData()
+    #print(dataFiles)
+    for data in dataFiles:
+        thread = open(data, 'r+', encoding="utf-8")
+        for word in thread.read().split():
+            word = word.lower()
             if word not in wordCount:
-               
-               wordCount[word] = 1
+                wordCount[word] = 1
             else:
-               
-               wordCount[word] +=1
+                wordCount[word] += 1
 
-   return wordCount
-         
+    return wordCount
+
+def analysis(dataForAnalysis):
+    wordCount = Counter(dataForAnalysis).most_common(n=None)
+    storeData = open('results.xml', 'w+', encoding="utf-8")
+    for item in wordCount:
+        storeData.write(str(item))
+        storeData.write('\n')
+
+def dataCleanup():
+    getList = listOfData()
+    print(getList)
+    for item in getList:
+        os.remove(item)
+        print(item+'.txt removed')
+
+dataGather()
+analysis(wordCounter())
+
+dataCleanup()
 
 
+# sortedWords = sorted(dataForAnalysis.keys())
+# for key in sorted(dataForAnalysis.keys()):
+#     print(key, " :: ", dataForAnalysis[key])
+# print(sortedWords)
 
-print(wordCounter())
-
-# loging = open('comments2.txt', 'w+',encoding="utf-8")
-# sub = reddit.submission(id='dk962x')
-# sub.comments.replace_more(limit=None)
-# for comment in sub.comments.list():
-
-#      #fullList.append(comment.body)
-#     # print(20 * "-")
-#      loging.write(str(20 * "-"))
-#      loging.write(str('\n'))
-#     # print('ID: ',comment.id)
-#      loging.write('ID: '+str(comment.id)+'\n')
-#      #print('Parent ID: ', comment.parent())
-#      loging.write('Parent ID: '+ str(comment.parent())+'\n')
-#      #print(comment.body)
-#      loging.write(str(comment.body)+'\n')
+#print(len(dataForAnalysis))
