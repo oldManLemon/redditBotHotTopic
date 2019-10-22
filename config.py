2ab1cfc4665ebfa714418b556851a5e268e72948
Andrew Hase <oldm8reggie@gmail.com>
2019-10-22 11:13:48 +0200

    restored config

---
+++ config.py
@@ -0,0 +1,11 @@
+# List of Basic configurations
+
+#Login Configs
+clientID='JL53WY5Cw2WK8g'
+clientSecret='fKqCZ44PIIFdqOjemTIKab0BfEs'              
+agent='popularWordBot Script'
+username='popularWordBot'
+#Action Considerations
+targetSub = 'melbourne' #consider making this a list
+limit = 25 #Sets the limit on hot posts to scan
+cleanUp = True # True deletes file False will Store them in a folder
--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from dataGather import data
 from analyse import analyseFunctions
 import config
 
+#Connect to Reddit and run the intial data collections
+#Results should be a bunch of txt files with thread titles and comments inside of them
 data.dataGather(5)
+#Here we scan each of the txt docs and count then number of time a word is used
+#This will need more cleaning to remove words that were missed at first
 analyseFunctions.analysis(analyseFunctions.wordCounter(), config.targetSub)
+#Here we either remove all the txt files or store them for future use. See config.pu
 data.dataCleanup(config.cleanUp, config.targetSub)
