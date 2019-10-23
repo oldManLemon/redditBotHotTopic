10bdb3a744fa6db51833bcc59b56d0269088a6de
Andrew <oldm8reggie@gmail.com>
2019-10-23 17:08:21 +0200

    basic error checking

--- dataGather/__init__.py
+++ dataGather/__init__.py
@@ -4,5 +4,7 @@ from dataGather.data import gatherRedditThreadsAndComments
 
 
 def gatherRedditData(limit):
+    if not type(limit) == int or not limit == 'n=None':
+        TypeError("Limits must be an int or n=None")
     gatherRedditThreadsAndComments(limit)
 
