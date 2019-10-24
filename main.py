eea7614f5a7e587b81ec40b7d3b98b870b7ab1cf
Andrew <oldm8reggie@gmail.com>
2019-10-24 13:03:40 +0200

    quick clean up to main

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ import config
 
 #Connect to Reddit and run the intial data collections
 #Results should be a bunch of txt files with thread titles and comments inside of them
-#dat.gatherRedditThreadsAndComments(config.limit)
+
 for item in config.targetSub:
 
     gatherRedditData(config.limit, item)
-    #Add more config options ehre
+    
 
-    #Here we scan each of the txt docs and count then number of time a word is used
-    #This will need more cleaning to remove words that were missed at first
-    #analyseFunctions.analysis(analyseFunctions.wordCounter(), config.targetSub)
-
+    #Here we scan each of the txt docs and count the total number of times a word is used
     runDataAnalysis(item)
 
-    #SQL Stuff
+    #SQL DataPush
     pushToDataBase(item)
 
 
@@ -25,8 +22,4 @@ for item in config.targetSub:
     cleanUp(config.cleanUp, item, 'txt') #Txt docs
 
     cleanUp(config.cleanUp, item, 'xml') #XML docs
-
-    #Here we either remove all the txt files or store them for future use. See config.pu
-    #dat.dataClean.dataCleanup(config.cleanUp, config.targetSub, 'txt')
-    #dat.dataClean.dataCleanup(config.cleanUp, config.targetSub, 'xml')
 
